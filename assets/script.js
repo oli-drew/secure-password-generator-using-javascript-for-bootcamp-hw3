0319739322a5b1251437da3eb30fb0c9fff11239
Oli Drew <oli.drew92@gmail.com>
2022-01-09 11:38:10 +0000

    Password length prompt and validation

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,9 @@
 // Assignment Code
 const generateBtn = document.querySelector("#generate");
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
 // Write password to the #password input
 function writePassword() {
   const password = generatePassword();
@@ -9,10 +12,40 @@ function writePassword() {
   passwordText.value = password;
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// Password length function
+// const passLengthFunc = () => {
+
+// }
 
 // Generate Password function
 function generatePassword() {
-  console.log("test");
+  // Ask user for desired password length
+  const passwordLength = window.prompt(
+    "Password length - Please enter a number between 8 and 128: "
+  );
+  if (passwordLength < 8 || passwordLength > 128) {
+    window.alert("Please enter a number between 8 and 128.");
+  } else {
+    console.log("perfect");
+  }
 }
+
+// Pseudo code
+// Click button then
+
+// open prompts for password criteria
+
+// prompts:
+// 1. Length 8 to 128 chars.
+
+// 2. lowercase
+
+// 3. uppercase
+
+// 4. numeric
+
+// 5. special
+
+// Validate At least one char type selected
+
+//
